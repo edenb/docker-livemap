b942735419814461a8db73d51f7a317c4acccba7
unknown <eribog@msn.com>
2016-10-13 21:38:19 +0200

    Fix initialization of database and role

--- postgres/init-user-db.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-    CREATE USER livemap;
-    CREATE DATABASE livemapdb;
-    GRANT ALL PRIVILEGES ON DATABASE livemapdb TO livemap;
-EOSQL
